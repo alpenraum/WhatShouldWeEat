ee5b3f1c8b0c173017f212a78e407a323ec0ad95
Finn Zimmer <finn@zimmers.at>
2020-10-16 10:56:06 +0200

    deleted unecessary using statement

--- Backend/WhatShouldWeEat/Startup.cs
+++ Backend/WhatShouldWeEat/Startup.cs
@@ -7,7 +7,6 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.EntityFrameworkCore;
-using WhatShouldWeEat.repository;
 using WhatShouldWeEat.Data;
 
 namespace WhatShouldWeEat
